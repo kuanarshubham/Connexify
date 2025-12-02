a6e625c09bd7d110ad8875dd72a2b8b0340651ea
Shubham <kuanarshubham@gmail.com>
2025-12-02 16:53:40 +0530

    2025-12-02 16:53:40

--- packages/webrtc-core/src/index.ts
+++ packages/webrtc-core/src/index.ts
@@ -1,5 +1,23 @@
-import  PeerService  from "./handlers/peerService";
+//import  PeerService  from "./handlers/peerService";
 import  Media  from "./handlers/mediaModule";
 
+class PeerService {
+    public peerConnection: RTCPeerConnection;
+    private config: RTCConfiguration = {
+        iceServers: [
+            {
+                urls: [
+                    "stun:stun.l.google.com:19302",
+                    "stun:global.stun.twilio.com:3478",
+                ],
+            },
+        ],
+    }
+
+    constructor() {
+        this.peerConnection = new RTCPeerConnection(this.config);
+    }
+
+}
 
 export { PeerService, Media };
